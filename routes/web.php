8cdecf0c070cef49d21219388ae874476a2388ab
alumno <alumno@vm1>
2021-11-29 19:39:15 +0100

    Esqueleto y primeras vistas

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/logout', function () {
+    return "Logout usuario";
+});
+
+
+Route::get('/productos', function () {
+    return view('productos.index');
+});
+
+Route::get('/productos/show/{id}', function ($id) {
+    return view('productos.show', array('id'=>$id));
+});
+
+Route::get('/productos/create', function () {
+    return view('productos.create');
+});
+
+Route::get('/productos/edit/{id}', function ($id) {
+    return view('productos.edit',array('id'=>$id)) ;
+});
